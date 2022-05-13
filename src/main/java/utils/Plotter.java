6d419d946fee135e91e76e4e3eddbabd995aaf15
danANDla <d.a.n.202021@gmail.com>
2022-05-13 21:17:50 +0300

    names of series is the same, cleam up Plotter class

--- src/main/java/utils/Plotter.java
+++ src/main/java/utils/Plotter.java
@@ -9,10 +9,12 @@ import org.knowm.xchart.style.lines.SeriesLines;
 import org.knowm.xchart.style.markers.SeriesMarkers;
 
 import javax.swing.*;
+
 public class Plotter {
 
     public Plotter() {
     }
+
     public void scatter(int counter, Point[] initial, Point[] points) {
         int width = 640;
         int height = 480;
@@ -20,9 +22,9 @@ public class Plotter {
 
         double[] xValues = new double[points.length];
         double[] yValues = new double[points.length];
-        String seriesname = "intital(" + counter + ")";
-        String name = "interpolated(" + counter + ")";
 
+        String seriesname = "intital";
+        String name = "interpolated";
         for (int i = 0; i < points.length; ++i) {
             xValues[i] = points[i].getX();
             yValues[i] = points[i].getY();
@@ -45,7 +47,5 @@ public class Plotter {
     private void show(XYChart chart) {
         JFrame frame = new SwingWrapper(chart).displayChart();
         frame.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
-        System.out.println("closed");
-//        frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
     }
 }
